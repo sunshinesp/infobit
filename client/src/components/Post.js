b8aa7eab215640df1bf4b96ba5f2f10af80ddf71
Surya Prakash <78016431+sunshinesp@users.noreply.github.com>
2024-08-26 15:09:30 +0530

    Add files via upload

---
+++ client/src/components/Post.js
@@ -0,0 +1,17 @@
+export default function Post () {
+    return (
+        <div className="post">
+            <div className="image">
+                <img src="https://e-uploads.franklinwh.com/website/news/889ff58b6a83bcb1b5059609d9f19c9d.jpg" alt="iamge"/>
+            </div>
+            <div className="text">
+                <h2>Protecting the Whole House with Home Battery Backup Systems</h2>
+                <p className="info">
+                    <a className="author" href="/surya">Surya Prakash</a>
+                    <time>24-09-2024 15:45</time>
+                </p>
+                <p className="summary">Energy is becoming more crucial to daily life as modern life depends on it more and more. We have more cars, bigger houses, more household appliances, and increased power needs. </p>
+            </div>
+        </div>
+    );
+}
